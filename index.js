17f6a7c36e4412ec5f4d0295dfc928715ed159d6
elian gonzalez <elianngonzalez7@gmail.com>
2022-03-10 23:54:26 -0300

    obtiene un solo recurso

--- index.js
+++ index.js
@@ -23,6 +23,12 @@ let notes = [{
 
 app.get('/', (req, res) => {
 	res.send('<h1>Hello World!</h1>');
+});
+
+app.get('/api/notes/:id', (request, response) => {
+	const id = request.params.id
+	const note = notes.find(note => note.id === id)
+	response.json(note)
 })
 
 app.get("/api/notes",(req, res) => {
