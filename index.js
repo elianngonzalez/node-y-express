4b539b32079f0c8af1ed47a1836dc92e23b0bee1
elian gonzalez <elianngonzalez7@gmail.com>
2022-03-11 21:52:20 -0300

    get persons & info

--- index.js
+++ index.js
@@ -22,14 +22,56 @@ let notes = [{
 	}
 ]
 
+let persons = [{
+			id: 1,
+			name: "Juan",
+			number: "123456789"
+		}, {
+			id: 2,
+			name: "Maria",
+			number: "987654321"
+		}, {
+			id: 3,
+			name: "Jose",
+			number: "123456789"
+		}, {
+			id: 4,
+			name: "Pedro",
+			number: "123456789"
+		}, {
+			id: 5,
+			name: "Juana",
+			number: "123456789"
+		},
+]
+
+
 app.get('/', (req, res) => {
 	res.send('<h1>Hello World!</h1>');
 });
 
 // GET /api/notes obtengo todas las notas
-app.get("/api/notes",(req, res) => {
-	res.json(notes);
+app.get("/api/persons",(req, res) => {
+	res.json(persons);
 });
+
+app.get("/api/persons/info", (req, res) => {
+	const total = persons.length;
+	const date = new Date();
+	const result = `<p>Total of persons: ${total}</p><br><p>${date}</p>`;
+	res.send(result);
+
+})
+
+app.get("/api/persons/:id", (req, res) => {
+	const id = Number(req.params.id);
+	const person = persons.find(pers => pers.id === id);
+	if (person) {
+		res.json(person);
+	} else {
+		res.status(404).end();
+	}
+})
 
 // GET /api/notes/:id obtengo una nota en especifico
 app.get('/api/notes/:id', (request, response) => {
@@ -63,7 +105,7 @@ app.post('/api/notes', (request, response) => {
 
 	if (!body.content) {
 		return response.status(400).json({
-			error: 'content missing'
+			error: 'sin contenido'
 		})
 	}
 
