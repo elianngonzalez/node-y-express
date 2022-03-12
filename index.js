dcc93ef94fe8e034def9c9822c30a659347f7c0e
elian gonzalez <elianngonzalez7@gmail.com>
2022-03-12 02:55:04 -0300

    morgam(logger de peticiones)

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const morgan = require('morgan')
 const app = express();
 app.use(express.json())
+
+
+
 
 
 
@@ -65,15 +69,19 @@ let persons = [{
 		},
 ]
 
-const requestLogger = (request, response, next) => {
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+/*const requestLogger = (request, response, next) => {
 	console.log('Method:', request.method)
 	console.log('Path:  ', request.path)
 	console.log('Body:  ', request.body)
 	console.log('---')
 	next()
 }
-
 app.use(requestLogger)
+*/
 
 app.get('/', (req, res) => {
 	res.send('<h1>Hello World!</h1>');
